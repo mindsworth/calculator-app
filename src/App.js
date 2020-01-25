77431b7a7e57c528ce5e959147f52e197a207bfb
Prince Goziem <princegoziem@gmail.com>
2020-01-25 06:11:09 +0100

    create some initial setup for the calc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
 
-function App() {
+const App = () => {
+  const [ result, setResult ] = useState('0');
+  const [ current, setCurrent ] = useState('0');
+  const [ previous, setPrevious ] = useState(0);
+
+  const reset = () => {
+    setResult('0');
+  };
+
+  const addToCurrent = symbol => {
+    setCurrent(current + symbol);
+  };
+  const buttons = [
+    { symbol: 'C', col: 3, action: reset },
+    { symbol: '/', col: 1, action: addToCurrent },
+    { symbol: '7', col: 1, action: addToCurrent },
+    { symbol: '8', col: 1, action: addToCurrent },
+    { symbol: '9', col: 1, action: addToCurrent },
+    { symbol: 'X', col: 1, action: addToCurrent },
+    { symbol: '4', col: 1, action: addToCurrent },
+    { symbol: '5', col: 1, action: addToCurrent },
+    { symbol: '6', col: 1, action: addToCurrent },
+    { symbol: '-', col: 1, action: addToCurrent },
+    { symbol: '1', col: 1, action: addToCurrent },
+    { symbol: '2', col: 1, action: addToCurrent },
+    { symbol: '3', col: 1, action: addToCurrent },
+    { symbol: '+', col: 1, action: addToCurrent },
+    { symbol: '0', col: 1, action: addToCurrent },
+    { symbol: '.', col: 1, action: addToCurrent },
+    { symbol: '=', col: 1, action: addToCurrent }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" className="calc-result" />
+      <div className="calc-btn-container">
+        {buttons.map((btn, index) => {
+          const { symbol, col, action } = btn;
+          return <Button symbol={symbol} col={col} action={(symbol) => action} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = props => {
+  return (
+    <div>
+      <button className="calc-btn">1</button>
+    </div>
+  );
+};
+
+export default Button;
